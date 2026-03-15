6941f0c3c59c09dc0da28bf5f61a679bda9e0b41
meme2wsx <meme2wsx@gmail.com>
2026-03-15 13:08:41 +0900

    event, cssの適用

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+const App = () => {
+  const onClickButton = () => {
+    alert('クリックされたよ！')
+  };
+
+  const contentStyle= {
+    color: "blue",
+    fontSize: "20px",
+  };
+
   return (
     <div>
-      <p>ここの段落は、App.jsxに書いているよ！</p>
+      {console.log("私は、パンダ🐼。consoleの住人であります。")}
+      <h1 style={{ color: "red" }}>赤です</h1>
+      <p style={contentStyle}>ここの段落は、App.jsxに書いているよ！</p>
+      <button onClick={onClickButton}>クリックしてね</button>
     </div>
   )
 }
-
 export default App
